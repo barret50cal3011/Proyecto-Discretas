506ecd966286c459431c862446a7d3b7748c5f41
unknown <barret50cal3011@gmail.com>
2025-07-19 02:24:10 -0500

    Started the creation of the table.

--- src/headers/util.hpp
+++ src/headers/util.hpp
@@ -11,3 +11,13 @@ bool isSingleLowerAlpha(const std::string& str);
 // @param latex The LaTeX formatted string to convert.
 // @return A string representing the logical expression.
 std::string latex_to_logic(const std::string& latex);
+
+// Converts an int into a binary vector representation.
+// @param int that will be converted.
+// @return A vector of strings, where each string is a binary digit of the integer.
+std::vector<std::string> int_to_binary(int number);
+
+// Creates a truth table from a given string representation of a logical expression.
+// @param str The string representation of the logical expression. String needs to have parentheses added.
+// @return A vector of vectors of strings, where each inner vector represents a row in the truth table.
+std::vector<std::vector<std::string>> create_table(const std::string& str);
--- src/util.cpp
+++ src/util.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <stdexcept>
+#include <algorithm>
 
 #include "util.hpp"
 
@@ -38,4 +39,46 @@ std::string latex_to_logic(const std::string& latex) {
         }
     }
     return logic;
+}
+
+std::vector<std::string> int_to_binary(int number) {
+    std::vector<std::string> binary_digits;
+    while (number > 0) {
+        binary_digits.push_back(std::to_string(number % 2));
+        number = number >> 1;
+    }
+
+    // Reverse the binary digits to get the correct order
+    std::reverse(binary_digits.begin(), binary_digits.end());
+
+    return binary_digits;
+}
+
+
+std::vector<std::vector<std::string>> create_table(const std::string& str) {
+
+    std::vector<std::vector<std::string>> table;
+    // find all the variables in the string
+    std::vector<std::string> variables;
+    for (char c : str) {
+        if (isSingleLowerAlpha(std::string(1, c)) && std::find(variables.begin(), variables.end(), std::string(1, c)) == variables.end()) {
+            variables.push_back(std::string(1, c));
+        }
+    }
+
+    std::vector<std::string> variable_row;
+    for (const auto& var: variables) {
+        variable_row.push_back(var);
+    }
+    table.push_back(variable_row); // Add the header row with variable names
+    
+    std::vector<std::vector<std::string>> table;
+    int num_rows = 1 << variables.size(); // 2^n rows for n variables
+
+    for (int i = 0; i < num_rows; ++i) {
+        std::vector<std::string> row;
+        std::vector<std::string> digits = int_to_binary(i);
+        row.insert(row.end(), digits.begin(), digits.end());
+        table.push_back(row);
+    }
 }
